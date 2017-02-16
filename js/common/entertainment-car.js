eb42beb37d0f4454dad43239c6c7aa7285ea7ce1
badger27 <s.freir@gmail.com>
2017-02-16 22:54:44 +0000

    data test

--- js/common/entertainment-car.js
+++ js/common/entertainment-car.js
@@ -1,4 +1,76 @@
-$(function() {
+$(document).ready(function(){
+
+
+
+
+  var imageNames = [ 1,2,3,4,5,6,7,8,9];
+  var imageLength = imageNames.length;
+  var directoryNames  = "movies";
+
+  getImages(directoryNames);
+
+
+
+  $("#movie").click(function(){
+
+    directoryNames  = "movies";
+    getImages(directoryNames);
+
+  });
+
+  $("#tv").click(function(){
+    directoryNames  = "tv";
+    getImages(directoryNames);
+  });
+
+
+  function getImages (directoryNames) {
+
+
+  if( directoryNames === "movies"){
+
+    for (var item = 0; item <imageLength ; item++) {
+     var  output= "../../img/" + directoryNames + "/" + imageNames[item] + ".jpg";
+
+      var result= "<li>" +   "<img src= "  +  output +  ">" + "</li>";
+
+      console.log("result " + result);
+      document.getElementById("demo").innerHTML += result;
+
+    }
+
+  } else if(directoryNames === "tv"){
+
+
+
+
+    var current=  $("li,img.sc-image" );
+    console.log("current" , current);
+
+    for (var i = 0; i <current.length ; i++) {
+      // console.log("array:" , current[i]);
+      $("#demo" ).empty($("#demo" ));
+    };
+
+    for (var item = 0; item <imageLength ; item++) {
+
+      // console.log("item " + item );
+      var  output= "../../img/" + directoryNames + "/" + imageNames[item] + ".jpg";
+
+
+      var result= "<li>" +   "<img src= "  +  output +  "" +  " class=" +  "'sc-image'" + " >" + "</li>";
+      console.log(" result", result);
+
+      $(".entertain .sky-carousel").carousel();
+      document.getElementById("demo").innerHTML += result;
+
+       }
+    }
+  }
+
+
+
+
   $(' .entertain .sky-carousel').carousel({
 
     itemWidth: 342,
@@ -19,43 +91,48 @@ $(function() {
   });
 
 
+
+
+
 });
 
 $(document).ready(function(){
 
 
-  $('.entertain').addClass('bg1');
 
-  $('.tv').hide();
-
-
-  $("#movie").click(function(){
-
-
-    $('.movie').fadeIn(300, function() { $(this).show(); })
-
-
-    $('.tv').hide().fadeOut(300, function() { $(this).hide(); });
-
-    $('.entertain').removeClass('bg2').addClass('bg1');
-
-
-  });
-
-  $("#tv").click(function(){
-
-    $('.tv').fadeIn(300, function() { $(this).show(); })
-
-
-    $('.movie').hide().fadeOut(300, function() { $(this).hide(); });
-
-
-
-
-
-    $('.entertain').removeClass('bg1').addClass('bg2');
-
-  });
+  //
+  // $('.entertain').addClass('bg1');
+  //
+  // // $('.tv').hide();
+  //
+  //
+  // $("#movie").click(function(){
+  //
+  //
+  //   $('.movie').fadeIn(300, function() { $(this).show(); })
+  //
+  //
+  //   $('.tv').hide().fadeOut(300, function() { $(this).hide(); });
+  //
+  //   $('.entertain').removeClass('bg2').addClass('bg1');
+  //
+  //
+  // });
+  //
+  // $("#tv").click(function(){
+  //
+  //   $('.tv').fadeIn(300, function() { $(this).show(); })
+  //
+  //
+  //   $('.movie').hide().fadeOut(300, function() { $(this).hide(); });
+  //
+  //
+  //
+  //
+  //
+  //   $('.entertain').removeClass('bg1').addClass('bg2');
+  //
+  // });
 
 });
 
