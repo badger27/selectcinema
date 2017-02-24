1af287c940524bf6c04008dde6d0333a09668030
badger27 <s.freir@gmail.com>
2017-02-24 12:11:48 +0000

    fix carousel

--- js/common/entertainment-car.js
+++ js/common/entertainment-car.js
@@ -43,6 +43,8 @@ $(document).ready(function(){
       navigationButtonsVisible :false,
       enableMouseWheel:false,
       selectByClick: true
+
+
     });
   }
 
@@ -65,6 +67,9 @@ $(document).ready(function(){
       navigationButtonsVisible :false,
       enableMouseWheel:false,
       selectByClick: true
+
+
+
     });
   }
 
@@ -74,26 +79,31 @@ $(document).ready(function(){
 
 
 
-  $("#movie").click(function(){
+  $("#movie").bind('click', function() {
 
-    $('.sky-carousel_first').remove();
+    $('.sky-carousel_first').fadeOut(300, function() { $(this).remove(); });
 
     getMovie();
-    $('.sky-carousel_movie').show();
     $('.sky-carousel_tv').hide();
+    $('.sky-carousel_movie').fadeIn(600, function() { $(this).show(); });
+
 
     $('.entertain').removeClass('bg2').addClass('bg1');
 
   });
 
 
-  $("#tv").click(function(){
+  $("#tv").bind('click', function() {
 
     $('.sky-carousel_first').remove();
 
     getTV();
+
+    // $('.movie').fadeIn(300, function() { $(this).show(); })
+    +    // $('.tv').hide().fadeOut(300, function() { $(this).hide(); });
+
     $('  .sky-carousel_movie').hide();
-    $('  .sky-carousel_tv').show();
+    $('  .sky-carousel_tv').fadeIn(600, function() { $(this).show(); });
 
 
     $('.entertain').removeClass('bg1').addClass('bg2');
